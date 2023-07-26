f0a729abb4d9fb2a92c1831bf14194feef5e14cc
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-07-26 10:43:46 -0600

    Performs the oxford comma algorithm for lists of items

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,22 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    if len(items) == 2:
+        return " and ".join(items)
+    return construct_multi_word_oxford_expression(items)
+
+def construct_multi_word_oxford_expression(items):
+    return_string = ""
+    for i in range(len(items)):
+        if (i < len(items) - 1):
+            return_string += items[i] + ", "
+        else:
+            return_string += "and " + items[i]
+        
+    return return_string
+
+
+print(oxford_comma(["Uno"]))
+print(oxford_comma(["Dick", "Jane"]))
+print(oxford_comma(["You", "Me", "Baby"]))
+print(oxford_comma(["George", "Ringo", "Paul", "John"]))
